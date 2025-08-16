869e0199e899fda5ea888091abd9fe12652ac549
Andrew De La Torre <drewdlt@yahoo.com>
2025-08-16 11:22:18 -0700

    finalized hero section of CharacterLookUp page and added Wolverine + Cyclops image files

--- src/components/SuperheroDetails.jsx
+++ src/components/SuperheroDetails.jsx
@@ -3,13 +3,13 @@ import { useEffect } from "react";
 function QuickInfo({ character }) {
   return (
     <>
-      <span className="block text-lg font-semibold tracking-wide">
+      <span className="block text-lg font-semibold tracking-wide xl:text-2xl">
         {character.real_name}
       </span>
-      <span className="block text-6xl font-extrabold tracking-wider sm:text-5xl">
+      <span className="block text-6xl font-extrabold tracking-wider sm:text-5xl xl:text-6xl">
         {character.name}
       </span>
-      <span className="block text-lg tracking-wide sm:text-base">
+      <span className="block text-lg tracking-wide sm:text-base xl:text-lg">
         {character.bio}
       </span>
     </>
@@ -21,9 +21,9 @@ function SuperheroDetails({ character }) {
     <>
       <div
         style={{ "--img-url": `url(${character.img})` }}
-        className="mt-4 flex h-95 w-full bg-[image:var(--img-url)] bg-cover bg-center sm:h-75 sm:items-center"
+        className="mt-4 flex h-95 w-full bg-[image:var(--img-url)] bg-cover bg-center sm:h-75 sm:items-center md:h-90 xl:h-130"
       >
-        <div className="ml-[5%] hidden h-fit gap-2 bg-gradient-to-r from-black/20 via-transparent to-transparent pl-2 sm:flex sm:w-1/3 sm:flex-col">
+        <div className="hidden h-fit bg-gradient-to-r from-black/45 via-black/30 to-transparent py-2 sm:pl-[10%] sm:h-full sm:justify-center xl:pl-[15%] sm:flex sm:w-1/2 sm:flex-col sm:gap-4 xl:w-1/2 xl:gap-4 transition delay-0 duration-150 ease-in hover:from-black/55 hover:via-black/30 hover:to-black/5 ">
           <QuickInfo character={character} />
         </div>
       </div>
